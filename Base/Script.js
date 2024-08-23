50b72c125fbbe16f62670de20208850be7d5b909
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-08-23 22:07:35 +0530

    In Progress
    -In Progress

--- Base/Script.js
+++ Base/Script.js
@@ -1,1 +1,24 @@
-console.log("Rock Paper Scissors");
+const choices=document.querySelectorAll(".icons");
+const newGame=document.querySelector(".newGame");
+const ngTitle=document.querySelector("#ng");
+const scoreBP=document.querySelector("#playerId");
+const scoreBC=document.querySelector("#comId");
+//topaz ai video enhancer 
+let player=0;
+let computer=0;
+
+
+
+
+choices.forEach((icons)=>{
+	icons.addEventListener("click",()=>{
+		let userChoiceId=icons.getAttribute("id");
+		ComputerChoice(userChoiceId);
+	});
+});
+
+let ComputerChoice=(userChoiceId)=>{
+	let userChoice=["rock","paper","scissors"];
+	let computerIdx=Math.floor(Math.random()*userChoice.length);
+	console.log(`User : ${userChoiceId} And ${userChoice[computerIdx]}`);
+};

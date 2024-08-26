409ffe34a5068796a97328bf49ab8fa569ec995e
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-08-26 22:05:40 +0530

    New Methods
    -Adding new methods and follow the Modular way of programming.

--- Base/Script.js
+++ Base/Script.js
@@ -6,19 +6,64 @@ const scoreBC=document.querySelector("#comId");
 //topaz ai video enhancer 
 let player=0;
 let computer=0;
-
+let selection=["rock","paper","scissors"];
 
 
 
 choices.forEach((icons)=>{
 	icons.addEventListener("click",()=>{
 		let userChoiceId=icons.getAttribute("id");
-		ComputerChoice(userChoiceId);
+		PlayGame(userChoiceId);
 	});
 });
 
-let ComputerChoice=(userChoiceId)=>{
-	let userChoice=["rock","paper","scissors"];
-	let computerIdx=Math.floor(Math.random()*userChoice.length);
-	console.log(`User : ${userChoiceId} And ${userChoice[computerIdx]}`);
-};
+let PlayGame=(userChoiceId)=>{
+	GenerateRandom(userChoiceId);
+}
+
+let GenerateRandom=(userChoiceId)=>{
+	let compChoice=Math.floor(Math.random()*selection.length);
+	console.log("UserSelection: ",userChoiceId);
+	console.log("ComputerSelection: ",selection[compChoice]);
+
+	
+	if(userChoiceId===selection[compChoice])
+	{	
+		console.log("Game Draw!!");
+	}
+	else if(userChoiceId==="rock"&&compChoice===1)
+	{
+		console.log("You Lose!");
+	}
+	else if(userChoiceId==="rock"&&compChoice===2)
+	{
+		console.log("You Lose!");
+	}
+	else if(userChoiceId==="paper"&&compChoice===2)
+	{
+		console.log("You Lose!");
+	}
+	else if(userChoiceId==="paper"&&compChoice===0)
+	{
+		console.log("You Won!");
+	}
+	else if(userChoiceId==="scissors"&&compChoice===0)
+	{
+		console.log("You Lose!");
+	}
+	else if(userChoiceId==="scissors"&&compChoice===2)
+	{
+		console.log("You Won!");
+	}
+	else
+	{
+		Draw(userChoiceId);
+	}
+}
+
+let Winner=()=>{
+	
+}
+let Lose=()=>{
+	
+}
